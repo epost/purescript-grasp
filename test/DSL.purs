603ad853b933678a55410c1a219c1eb6049438b9
Erik Post <erik@shinsetsu.nl>
2019-01-02 23:06:06 +0100

    Move graph construction DSL into separate test file.

---
+++ test/DSL.purs
@@ -0,0 +1,35 @@
+-- |  Some very rough and experimental bits of DSL to facilitate graph construction.
+module Test.Language.Grasp.DSL where
+
+import Prelude
+import Data.Either (Either(..), either)
+import Data.List (many, fromFoldable)
+import Data.Maybe (Maybe(..))
+import Data.Tuple (Tuple(..))
+import Data.Tuple.Nested (type (/\), (/\))
+import Language.Grasp.AST (Node(..), Edge(..), GElem1(..), Label, Type, LabelAndType)
+
+-- | Construct an untyped node.
+node l = Node (l : Nothing)
+
+oftype :: Label -> Maybe Type -> LabelAndType
+oftype l t = l /\ t
+infixl 1 oftype as :
+
+typepatch (Node (l /\ t)) t' = Node (l /\ t')
+infixl 1 typepatch as :::
+
+n1 =            node
+n2 = GNode1 <<< node
+n  = n2
+
+nt :: String -> String -> GElem1
+nt l t = GNode1 $ Node (l : Just t)
+
+to1     =          Edge Nothing
+to2 x y =          Edge Nothing (node x) (node y)
+to3 x y = GEdge1 $ Edge Nothing (node x) (node y)
+
+infixl 4 to1 as ~>
+infixl 4 to2 as ~~>
+infixl 4 to3 as ~~~>
--- test/Main.purs
+++ test/Main.purs
@@ -8,8 +8,8 @@ import Data.List (many, fromFoldable)
 import Data.Maybe (Maybe(..))
 import Data.Map as Map
 import Data.Tuple (Tuple(..))
-import Data.Tuple.Nested ((/\))
-import Language.Grasp.AST (Node(..), Edge(..), GElem1(..), Label, Type, LabelAndType)
+import Data.Tuple.Nested (type (/\), (/\))
+import Language.Grasp.AST (Node(..), Edge(..), GElem1(..), Label, Type, LabelAndType, NodeStyleRec)
 import Language.Grasp.Generator.GraphViz as GraphViz
 import Language.Grasp.Generator.PlantUML as PlantUML
 import Language.Grasp.Parser as Parser
@@ -19,6 +19,7 @@ import Test.Spec                  (describe, pending, it)
 import Test.Spec.Runner           (run)
 import Test.Spec.Assertions       (shouldEqual)
 import Test.Spec.Reporter.Console (consoleReporter)
+import Test.Language.Grasp.DSL
 
 main :: _
 main = run [consoleReporter] do
@@ -70,39 +71,9 @@ main = run [consoleReporter] do
         `shouldEqual`
         ("@startuml\nactor \"x\"\n\"x\" -> \"y\"\n" <> "\"y\" -> \"z\"\n@enduml")
 
-
+styleEnv :: Array (String /\ NodeStyleRec) -> String -> Maybe NodeStyleRec
 styleEnv = flip Map.lookup <<< Map.fromFoldable
-
 
 itParses str p exp = it ("should parse: \"" <> str <> "\" as " <> show exp) $ (runParser str p) `shouldParseTo` exp
 
 shouldParseTo v exp = shouldEqual v (Right exp)
-
---
--- Some very rough and experimental bits of DSL to facilitate graph construction.
---
-
--- | Construct an untyped node.
-node l = Node (l : Nothing)
-
-oftype :: Label -> Maybe Type -> LabelAndType
-oftype l t = l /\ t
-infixl 1 oftype as :
-
-typepatch (Node (l /\ t)) t' = Node (l /\ t')
-infixl 1 typepatch as :::
-
-n1 =            node
-n2 = GNode1 <<< node
-n  = n2
-
-nt :: String -> String -> GElem1
-nt l t = GNode1 $ Node (l : Just t)
-
-to1     =          Edge Nothing
-to2 x y =          Edge Nothing (node x) (node y)
-to3 x y = GEdge1 $ Edge Nothing (node x) (node y)
-
-infixl 4 to1 as ~>
-infixl 4 to2 as ~~>
-infixl 4 to3 as ~~~>
