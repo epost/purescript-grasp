0c167004f15f859eda64fa4a1798a88a8f0fe467
Erik Post <erik@shinsetsu.nl>
2019-10-24 23:28:03 +0200

    Rearrange and comment AST code.

--- src/Language/Grasp/AST.purs
+++ src/Language/Grasp/AST.purs
@@ -8,36 +8,45 @@ import Data.Foldable (foldMap)
 
 type Label = String
 
+-- | This captures labels with (type) annotations, which ay look something like `x:a`.
 type LabelAndType = Label /\ (Maybe Type)
 
 type Type = String
 
-data NodeF a = Node a
 type Node = NodeF LabelAndType
 
-data MultiEdgeF f l s t = MultiEdge (Maybe l) (f (NodeF s)) (f (NodeF t))
-type MultiEdge          = MultiEdgeF List LabelAndType LabelAndType LabelAndType
+type MultiEdge = MultiEdgeF List LabelAndType LabelAndType LabelAndType
+
+-- | Graph element type. A graph is a bunch of nodes and edges, and can be be
+-- | represented as a `List GElem1` for example.
+type GElem1 = GElem1F LabelAndType
+
+--------------------------------------------------------------------------------
+
+newtype NodeF a = Node a
+
+unNode :: forall f a. NodeF a -> a
+unNode (Node x) = x
 
 derive instance eqNodeF :: Eq a => Eq (NodeF a)
 
 instance showNodeF :: Show a => Show (NodeF a) where
   show (Node x) = "(NodeF " <> show x <> ")"
 
+--------------------------------------------------------------------------------
+
+data MultiEdgeF f l s t = MultiEdge (Maybe l) (f (NodeF s)) (f (NodeF t))
+
 instance showMultiEdgeF :: (Show l, Show s, Show t) => Show (MultiEdgeF List l s t) where
   show (MultiEdge l s t) = "(MultiEdge " <> show l <> " " <> show s <> " " <> show t <> ")"
 
 derive instance eqMultiEdgeF :: (Eq l, Eq s, Eq t) => Eq (MultiEdgeF List l s t)
 
-unNode :: forall f a. NodeF a -> a
-unNode (Node x) = x
-
 --------------------------------------------------------------------------------
 
+-- | Generic graph element type.
 data GElem1F a = GNode1      (NodeF a)
                | GMultiEdge1 (MultiEdgeF List a a a)
-
--- | A graph can be be represented by `List GElem1`.
-type GElem1 = GElem1F LabelAndType
 
 derive instance eqGElem1 :: Eq a => Eq (GElem1F a)
 
