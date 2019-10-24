3a80e152c92577c072226227328b839f535d82a2
Erik Post <erik@shinsetsu.nl>
2019-10-24 23:02:15 +0200

    Upgrade to PureScript 0.13.4.

--- src/Language/Grasp/Generator.purs
+++ src/Language/Grasp/Generator.purs
@@ -1,9 +1,8 @@
 module Language.Grasp.Generator where
 
 import Prelude ((<>))
-import Language.Grasp.AST
 import Language.Grasp.Stylesheet.AST as Stylesheet
-import Language.Grasp.Stylesheet.AST (Selector, SelectorElem(..))
+import Language.Grasp.Stylesheet.AST (Selector)
 import Data.Maybe (Maybe(..))
 
 type Styler = Selector -> Maybe Stylesheet.Attrs
